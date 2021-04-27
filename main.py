1bd26374aef2bbd4f41ef82a4ac9bca60e74bae6
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2021-04-28 00:58:35 +0200

    reformat main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import PyPDF2
 if __name__ == '__main__':
     ll = ["abbrev", "adj", "adv", "av", "Am", "Eng", "Br", "conj", "det", " exclam", "mv", "n", "phrv", "pi", "prep",
           "prep", "phr" "pron", "sing", "v"]
-    words="words directory"
+    words = "words directory"
     root_dir = os.path.abspath(os.curdir)
     path = os.path.join(root_dir, words)
-    path1=os.path.join(path,"extract-words-from-pdf.txt")
+    path1 = os.path.join(path, "extract-words-from-pdf.txt")
     if not os.path.exists(path):
         os.mkdir(path)
     regex = re.compile('[^a-zA-Z]')
@@ -21,13 +21,13 @@ if __name__ == '__main__':
         txt = pageObj.extractText().split(" ")
         l = []
         for i in txt:
-            pp=[]
-            xc=i.find('/n')
-            if xc>-1:
-                i=i.replace('/n','')
-            cv=i.find('/')
-            if cv>-1:
-                pp=i.split('/')
+            pp = []
+            xc = i.find('/n')
+            if xc > -1:
+                i = i.replace('/n', '')
+            cv = i.find('/')
+            if cv > -1:
+                pp = i.split('/')
             else:
                 pp.append(i)
             for ppp in pp:
@@ -42,6 +42,5 @@ if __name__ == '__main__':
             # if dict[i] is not None:
             dict[i] = 1
         print(dict.keys())
-        f.write(" ".join(dict.keys())+'\n')
+        f.write(" ".join(dict.keys()) + '\n')
         f.close()
-
